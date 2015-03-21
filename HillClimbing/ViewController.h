b46d4e1bd17d9961868cffeb7b663491d9cbf1fb
Pedro <claudiosantos@Macbook.local>
2015-03-20 23:28:28 -0300

    Initial Commit

---
+++ HillClimbing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HillClimbing
+//
+//  Created by Claudio Santos on 3/20/15.
+//  Copyright (c) 2015 Claudio Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HillClimbing/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HillClimbing
+//
+//  Created by Claudio Santos on 3/20/15.
+//  Copyright (c) 2015 Claudio Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
